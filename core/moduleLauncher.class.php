731ccd3d8b2723165ec481cf22a5da9ff19eda05
fliege01 <fliege01@users.noreply.github.com>
2015-03-12 20:52:28 +0100

    Update moduleLauncher.class.php

--- core/moduleLauncher.class.php
+++ core/moduleLauncher.class.php
@@ -12,6 +12,9 @@ class moduleLauncher {
           if(file_exists(ROS_MODULES . $module .'/index.php')){
             require_once ROS_MODULES . $module .'/index.php';
             if(class_exists($module)){
+            		 if($this->reg->keyExists($module)){
+            		 	
+            		 }
 			  $module_data = 'm_'.$module;
 			  $module_call = 'call_'.$module;
               $this->reg->$module_data = new $module;
