45b95b108eee3408c430bea8510dd27911716d96
fliege01 <fliege01@users.noreply.github.com>
2015-03-12 19:25:13 +0100

    Update core.php

--- core/core.php
+++ core/core.php
@@ -1,5 +1,27 @@
 <?php
-// RegattaOS Core
-// Version: 0.2
-// Author: Nico Bischof
+require_once 'config.php';
+require_once 'registry.class.php';
+$reg = Registry::getInstance();
+// Sez Registry
+$reg->config = $config;
+$reg->permissionsarray = array();
+$reg->menu = array();
+$reg->call = array();
+$reg->publicParameter = array();
+$reg->pdo = new PDO('mysql:host='.$config['mysql_host'].';dbname='.$config['mysql_db'].';charset=utf8', $config['mysql_user'], $config['mysql_pass']);
+require_once 'moduleCore.class.php';
+require_once 'moduleLauncher.class.php';
+$launcher = new moduleLauncher;
+
+if(isset($_GET['m'])){
+	
+if(isset($reg->call[$_GET['m']])){
+	$cf = 'm_'.$_GET['m'];
+	$modfunc = $reg->call[$_GET['m']];
+	$module = $reg->$cf;
+	$module->$modfunc();
+}
+
+}
+
 ?>
