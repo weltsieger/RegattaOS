d360660e97cd41dfbbffa4250db4ab98290f413f
fliege01 <fliege01@users.noreply.github.com>
2015-03-12 19:25:37 +0100

    Create config.php

---
+++ core/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+"mysql_db" => "rosrframework",
+"mysql_user" => "ros",
+"mysql_pass" => "ros2015",
+"mysql_host" => "localhost",
+"publicfiles" => "/public/",
+"title" => "Blockspot"
+);
+?>
